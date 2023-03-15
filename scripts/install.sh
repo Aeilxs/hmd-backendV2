af15b14f3b59a0874db025b1d139611e98a485ef
Alexis D <dutaud.alex@gmail.com>
2023-03-15 15:09:28 +0100

    feat: updated scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ install()
   symfony console doctrine:database:create
   symfony console doctrine:schema:create
   symfony console lexik:jwt:generate-keypair
+  symfony console do:fi:lo
 }
 
 install
--- scripts/resetDB.sh
+++ scripts/resetDB.sh
@@ -5,6 +5,7 @@ resetDB()
   symfony console doctrine:database:drop --force
   symfony console doctrine:database:create
   symfony console doctrine:schema:create
+  symfony console do:fi:lo
 }
 
 resetDB
